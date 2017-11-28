1bf93c0aed5b1b5c812a86e395e133b79bffe2d1
frhnevrzl <farhanevirzal15@gmail.com>
2017-11-28 18:30:04 +0700

    baru

---
+++ ReadExcelFile/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using ExcelDataReader;
+
+namespace ReadExcelFile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        DataSet result;
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            using(OpenFileDialog ofd = new OpenFileDialog() { Filter="Excel WorkBook | *.xlsx", ValidateNames = true })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    FileStream fs = File.Open(ofd.FileName, FileMode.Open, FileAccess.Read);
+                    IExcelDataReader reader = ExcelReaderFactory.CreateOpenXmlReader(fs);
+                    result = reader.AsDataSet();
+                    cboSheet.Items.Clear();
+                    foreach (DataTable dt in result.Tables)
+                        cboSheet.Items.Add(dt.TableName);
+                    reader.Close();
+                }
+            }
+        }
+
+        private void cboSheet_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dataGridView.DataSource = result.Tables[cboSheet.SelectedIndex];
+        }
+    }
+}
